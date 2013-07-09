3f0130bbc98335b179234f629bd331606179468e
Ngo Trung Nhan <ntnhan@gmail.com>
2013-07-09 15:00:11 +0700

    add test mrbig

--- hello.c
+++ hello.c
@@ -6,6 +6,7 @@ static char* trim(char* p)
 {
 	while ( isspace(*p) )  p++;
 	int len = strlen(p);
+
 	while ( isspace(p[len - 1])) len--;
 	p[len] = '\0';
 	return p;
@@ -14,5 +15,9 @@ static char* trim(char* p)
 int main(int argc, char* argv[])
 {
 	printf("Hello world \n");
+	
+	char p[] = " Mr Big ";
+	printf("%s\n", trim(p));
+
 	return 0;
 }
